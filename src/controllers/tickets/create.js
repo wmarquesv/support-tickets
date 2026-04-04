9fa5643a5d924e9d4916b301ad28d3fb2f0ffaf6
wmarquesv <walace.mv@hotmail.com>
2026-04-03 22:29:36 -0300

    criado o objeto ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,3 +1,17 @@
+import {randomUUID} from "node:crypto"
+
 export function create({request, response}){
-    return response.end("Criado com sucesso!")
+    const { equipament, description, user_name} = request.body
+
+    const ticket = {
+        id: randomUUID(),
+        equipament,
+        description,
+        user_name,
+        status: "open",
+        created_at: new Date(),
+        updated_at: new Date()
+    }
+
+    return response.end(JSON.stringify(ticket))
 }
