1ee3d64f97b8554ead8bded46739be7cf55f5939
wmarquesv <walace.mv@hotmail.com>
2026-04-03 22:48:01 -0300

    criado banco de dados

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,4 +1,7 @@
 import {routes} from "../routes/index.js"
+import { Database } from "../database/database.js" 
+
+const database = new Database()
 
 export function routeHandler(request, response){
 const route = routes.find((route) => {
@@ -6,7 +9,7 @@ const route = routes.find((route) => {
 })
 
 if(route){
-    return route.controller({request, response})
+    return route.controller({request, response, database})
 }
 
 return response.writeHead(404).end()
