13b522881ad9dfd403fba6d925a7f2c2cdcdeb0c
wmarquesv <walace.mv@hotmail.com>
2026-04-07 19:32:19 -0300

    adicionado remocao de tickets

--- src/controllers/tickets/updateStatus.js
+++ src/controllers/tickets/updateStatus.js
@@ -1,6 +1,8 @@
 export function updateStatus({request, response, database}){
     const {id} = request.params
-    database.update("tickets", id, {status: "closed"})
+    const {solution} = request.body
+
+    database.update("tickets", id, {status: "closed", solution})
     
     return response.end()
 }
--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -2,6 +2,7 @@ import {create} from "../controllers/tickets/create.js"
 import { index } from "../controllers/tickets/index.js"
 import { update } from "../controllers/tickets/update.js"
 import { updateStatus } from "../controllers/tickets/updateStatus.js"
+import { remove } from "../controllers/tickets/remove.js"
 
 export const tickets = [
 {
@@ -23,5 +24,10 @@ export const tickets = [
     method: "PATCH",
     path: "/tickets/:id/close",
     controller: updateStatus,
+},
+{
+    method: "DELETE",
+    path: "/tickets/:id",
+    controller: remove,
 }
 ]
