8a2117d24d639ace87314ee6b4793203edc63690
wmarquesv <walace.mv@hotmail.com>
2026-04-07 18:43:28 -0300

    atualizacao do ticket adicionada

--- src/database/database.js
+++ src/database/database.js
@@ -43,4 +43,17 @@ export class Database{
         
         return data
     }
+
+    update(table, id, data){
+        const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+        if(rowIndex > -1){
+            this.#database[table][rowIndex] ={
+                ...this.#database[table][rowIndex],
+                ...data
+            }
+
+            this.#persist()
+        }
+    }
 }
